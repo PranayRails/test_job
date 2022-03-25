3c0fe64f59997e86d991a49cccc04bb6ed5679af
Hitendra <htwebdev331@gmail.com>
2022-03-25 18:50:29 +0530

    added login signup feature

--- parcel-scheduling-system/app/controllers/application_controller.rb
+++ parcel-scheduling-system/app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+    def after_sign_out_path_for(resource)
+      new_user_session_path
+    end
+
+    def after_sign_in_path_for(resource)
+      home_index_path
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:account_update, keys: %i[name email password])
+      devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password type])
+    end
 end
