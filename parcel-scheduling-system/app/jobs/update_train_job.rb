66b31a20f6d0c2a23cf19ac0c97e59a444950ed2
Hitendra <htwebdev331@gmail.com>
2022-03-28 20:02:54 +0530

    added update train job

---
+++ parcel-scheduling-system/app/jobs/update_train_job.rb
@@ -0,0 +1,21 @@
+class UpdateTrainJob
+   queue_as :default
+
+  def perform(train_booking_id)
+    train_booking = TrainBooking.find_by_id(train_booking_id)
+    train = train_booking.train
+    parcels = train_booking.parcels
+
+    # update train status
+    train.update(status: "reached")
+
+    # swap source and destination
+    train.update(source: train.destination, destination: train.source)
+
+    # update parcels status
+    parcels.update(status: "delivered")
+
+    # update train booking status
+    train_booking.update(status: "complete")
+  end
+end
