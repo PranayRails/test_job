d906148e4a5b78e5fa84c56c61fd9cf71eb46377
Hitendra <htwebdev331@gmail.com>
2022-03-29 02:11:59 +0530

    added symbols

--- parcel-scheduling-system/db/seeds.rb
+++ parcel-scheduling-system/db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-TrainOperator.create(
+train_operator = TrainOperator.create(
   name: "Train Operator",
   email: "train-operator@yopmail.com",
   password: "train-operator")
@@ -20,3 +20,28 @@ ParcelOwner.create(
   name: "Parcel Owner",
   email: "parcel-owner@yopmail.com",
   password: "parcel-owner")
+
+Train.create(
+  source: "A",
+  destination: "B",
+  weight_capacity: 500,
+  volume_capacity: 8,
+  cost: 200,
+  train_operator_id: train_operator.id
+  )
+Train.create(
+  source: "B",
+  destination: "C",
+  weight_capacity: 150,
+  volume_capacity: 1,
+  cost: 150,
+  train_operator_id: train_operator.id
+  )
+Train.create(
+  source: "A",
+  destination: "C",
+  weight_capacity: 300,
+  volume_capacity: 4,
+  cost: 300,
+  train_operator_id: train_operator.id
+  )
