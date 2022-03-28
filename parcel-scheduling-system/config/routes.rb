c29e1cbbbc6f70e21dbb40edabf7089d36621f98
Hitendra <htwebdev331@gmail.com>
2022-03-28 20:22:23 +0530

    remove unnecessary files

--- parcel-scheduling-system/config/routes.rb
+++ parcel-scheduling-system/config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
   resources :train_bookings
   resources :parcels
   resources :post_masters
   resources :trains
-  get 'home/index'
-  devise_for :users
-  root to: "home#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
