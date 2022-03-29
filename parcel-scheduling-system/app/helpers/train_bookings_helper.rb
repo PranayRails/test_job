5597e548a1742df9b75d25c0dc278360d11d6943
Hitendra <htwebdev331@gmail.com>
2022-03-29 17:59:10 +0530

    container fixes

--- parcel-scheduling-system/app/helpers/train_bookings_helper.rb
+++ parcel-scheduling-system/app/helpers/train_bookings_helper.rb
@@ -1,7 +1,2 @@
 module TrainBookingsHelper
-  def get_trains
-  	parcels = Parcel.where(id: @parcel_ids)
-  	trains = Train.where(source: parcels.first.source, destination: parcels.first.destination)
-	  trains.available_for_parcels(@parcel_ids)
-  end
 end
