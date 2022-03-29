1082fd4540f70e31de3ae67c693ec7cc246041b6
Hitendra <htwebdev331@gmail.com>
2022-03-29 20:18:54 +0530

    added validations for user

--- parcel-scheduling-system/app/models/user.rb
+++ parcel-scheduling-system/app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  USER_TYPES = ["PostMaster", "ParcelOwner", "TrainOperator", nil]
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validate :validate_type
 
   def train_operator?
     self.type == 'TrainOperator'
@@ -14,5 +18,9 @@ class User < ApplicationRecord
 
   def post_master?
     self.type == 'PostMaster'
+  end
+
+  def validate_type
+    errors.add(:type, "Subclass Not Found") unless User::USER_TYPES.include?(self.type)
   end
 end
